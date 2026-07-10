74becab5b814ffd7fe7b8b2369b30c609a649c40
Leimingyu <7437302@qq.com>
2026-07-10 12:03:48 +0800

    未改动文件提交

---
+++ src/back-end/HSBG.BidManager/Areas/Admin/Pages/BidManager/Index.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using Senparc.Ncf.Service;
+using System;
+
+namespace BidManager.Areas.BidManager.Pages
+{
+    public class Index : Senparc.Ncf.AreaBase.Admin.AdminXncfModulePageModelBase
+    {
+        public Index(Lazy<XncfModuleService> xncfModuleService) : base(xncfModuleService)
+        {
+
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ src/back-end/HSBG.BidManager/Domain/Models/DatabaseModel/Mapping/BidManager_ColorConfigurationMapping.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Senparc.Ncf.Core.Models.DataBaseModel;
+using Senparc.Ncf.XncfBase.Attributes;
+
+namespace BidManager.Models
+{
+    [XncfAutoConfigurationMapping]
+    public class BidManager_ColorConfigurationMapping : ConfigurationMappingWithIdBase<Color, int>
+    {
+        public override void Configure(EntityTypeBuilder<Color> builder)
+        {
+            builder.Property(e => e.Red).IsRequired();
+            builder.Property(e => e.Green).IsRequired();
+            builder.Property(e => e.Blue).IsRequired();
+        }
+    }
+}
